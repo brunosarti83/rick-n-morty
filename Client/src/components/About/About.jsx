3667a4e55b7ecd45dfb4822791d64194d6f4a33c
brunosarti83 <brunosarti.bs@gmail.com>
2023-07-10 18:56:23 -0300

    updated responsive design

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -11,7 +11,18 @@ export default function About() {
         <div className={styles.container}>
             <h1>Soy Bruno 🖐</h1>
             <div className={styles.grid}>
-                <div className={styles.dataColumn}></div>
+                <div className={styles.dataColumn}>
+                    < div className={styles.text}>
+                        <h2><ion-icon name="logo-github"></ion-icon> /brunosarti83</h2>
+                        <div className={styles.linkedin}>
+                            <p>Estudiante en Henry <ion-icon name="school"></ion-icon> FullStack Developer<br></br>
+                            Javascript / React / HTML / CSS / Express / PostgreSQL<br></br>
+                            
+                            </p>  
+                            <h2><ion-icon name="logo-linkedin"></ion-icon> /brunosarti83</h2>
+                        </div>
+                    </div>
+                </div>
                 <div className={styles.imgDiv}>
                     <div className={styles.image}></div>
                 </div>
--- Client/src/components/NavBar/NavBar.jsx
+++ Client/src/components/NavBar/NavBar.jsx
@@ -1,45 +1,66 @@
 // import styles
 import styles from './NavBar.module.css';
 // import hooks
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 // import helpers
 import { ROUTES } from '../../helpers/ROUTES';
 
 export default function NavBar() {
 
     const user = useSelector((state) => state.user)
+    const [showMenu, setShowMenu] = useState(false)
+    const location = useLocation()
+
+    useEffect(() => {
+        setShowMenu(false)
+    },[location])
+
+    const handleMenu = (event) => {
+        setShowMenu(!showMenu)
+    }
 
     return (
-        <div className={styles.navDiv}>
-            <ul className={styles.list}>
-                <NavLink to={ROUTES.home} className={({ isActive }) => isActive ? styles.activeLink : styles.pendingLink}>
+        <div className={styles.navBar}>
+            <div className={showMenu ? styles.hiddenDiv : styles.notShowing} onClick={handleMenu}></div>
+            <div className={styles.logoUsefull} onClick={handleMenu}></div>
+            <div className={!showMenu ? styles.navDiv : styles.navDivAlt}>
+                <ul className={styles.list}>
+                    <NavLink to={ROUTES.home} className={({ isActive }) => isActive ? styles.activeLink : styles.pendingLink}>
+                        <li>
+                            <div className={styles.liWrap}>
+                                <div className={styles.marker}><span>HOME</span></div>
+                            </div>
+                        </li>
+                    </NavLink>
+                    <NavLink to={ROUTES.about} className={({ isActive }) => isActive ? styles.activeLink : styles.pendingLink}>
+                        <li>
+                            <div className={styles.liWrap}>
+                                <div className={styles.marker}><span>ABOUT</span></div>
+                            </div>
+                        </li>
+                    </NavLink>
+                    <NavLink to={ROUTES.favorites} className={({ isActive }) => isActive ? styles.activeLink : styles.pendingLink}>
+                        <li>
+                            <div className={styles.liWrap}>
+                                <div className={styles.marker}><span>FAVORITES</span></div>
+                            </div>
+                        </li>
+                    </NavLink>
                     <li>
                         <div className={styles.liWrap}>
-                            <div className={styles.marker}><span>Home</span></div>
+                            <span className={styles.user}>{user}</span>
                         </div>
                     </li>
-                </NavLink>
-                <NavLink to={ROUTES.about} className={({ isActive }) => isActive ? styles.activeLink : styles.pendingLink}>
-                    <li>
-                        <div className={styles.liWrap}>
-                            <div className={styles.marker}><span>About</span></div>
-                        </div>
-                    </li>
-                </NavLink>
-                <NavLink to={ROUTES.favorites} className={({ isActive }) => isActive ? styles.activeLink : styles.pendingLink}>
-                    <li>
-                        <div className={styles.liWrap}>
-                            <div className={styles.marker}><span>Favorites</span></div>
-                        </div>
-                    </li>
-                </NavLink>
-            </ul>
-            <div className={styles.userSection}>
-                <span className={styles.user}>{user}</span>
-                <NavLink to={ROUTES.landing} className={styles.logout}>
-                    <span>Log out</span>
-                </NavLink>
+                    <NavLink to={ROUTES.landing} className={styles.logout}>
+                        <li>
+                            <div className={styles.liWrap}>
+                                <div className={styles.marker}><span>LOG OUT</span></div>
+                            </div>
+                        </li>
+                    </NavLink>
+                </ul>
             </div>
         </div>
     )
