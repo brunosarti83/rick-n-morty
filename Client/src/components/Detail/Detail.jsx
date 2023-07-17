33e69999b66cc51f9bf5290d8f38392f243d7e73
brunosarti83 <brunosarti.bs@gmail.com>
2023-07-16 23:33:02 -0300

    fixing mobile

--- Client/src/components/Detail/Detail.jsx
+++ Client/src/components/Detail/Detail.jsx
@@ -27,7 +27,7 @@ export default function Detail () {
                     <div className={styles.status}><h3>Status:<br></br>{character?.status}</h3></div>
                     <div className={styles.species}><h3>Species:<br></br>{character?.species}</h3></div>
                     <div className={styles.gender}><h3>Gender:<br></br>{character?.gender}</h3></div>
-                    <div className={styles.origin}><h3>Origin:<br></br>{character?.origin?.name}</h3></div>
+                    <div className={styles.origin}><h3>Origin:<br></br>{character?.origin}</h3></div>
                     <div className={styles.photo} style={{background: `url(${character.image}) no-repeat`}}></div>
                     <div className={styles.action}>
                         <span>Act now!</span>
